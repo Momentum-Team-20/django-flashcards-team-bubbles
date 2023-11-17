61e7f5c96d594e14b8032ae3611f763a64a62caa
jmatthews64 <jon.matthews64@gmail.com>
2023-11-17 17:06:01 -0500

    Added views and templates to see User's boxes on site
    Co-authored by: jmatthews64 jon.matthews64@gmail.com
    Co-authored by: jqs787 qjoseantonio@gmail.com

--- study_with_cards/views.py
+++ study_with_cards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import User, Card_Box, Card, Deck
+
 
 # Create your views here.
+def user_home(request):
+    boxes = Card_Box.objects.all()
+    return render(request, 'index.html', {'boxes': boxes})
